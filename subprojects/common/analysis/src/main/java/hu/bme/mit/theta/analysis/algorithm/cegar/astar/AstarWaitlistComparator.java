f8d50978580bdeaf264c784e3691c5784eaf44d5
asztrikx <asztrik35@gmail.com>
2022-09-18 00:54:49 +0200

    remove comments

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/AstarWaitlistComparator.java
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/AstarWaitlistComparator.java
@@ -8,20 +8,6 @@ import hu.bme.mit.theta.analysis.algorithm.cegar.astar.AstarSearch.Edge;
 import java.util.Comparator;
 
 public class AstarWaitlistComparator<S extends State, A extends Action> implements Comparator<Edge<S, A>> {
-	/*
-		// in astarExtend we do not have INFINITE nodes so this part is only for generality
-		final boolean unreachable1 = weight1.getType() == AstarNode.DistanceType.INFINITE;
-		final boolean unreachable2 = weight2.getType() == AstarNode.DistanceType.INFINITE;
-
-		if (unreachable1 && unreachable2) {
-			return 0;
-		} else if (unreachable1) {
-			return 1;
-		} else if (unreachable2) {
-			return -1;
-		}
-	*/
-
 	@Override
 	public int compare(Edge<S, A> edge1, Edge<S, A> edge2) {
 		AstarNode<S, A> astarNode1 = edge1.end;
