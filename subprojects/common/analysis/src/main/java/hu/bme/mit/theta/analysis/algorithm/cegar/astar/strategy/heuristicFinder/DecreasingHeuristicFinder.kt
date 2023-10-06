009daa1df07d0fd6623a3494f3aab58a9c4ff932
asztrikx <asztrik35@gmail.com>
2023-10-06 16:48:24 +0200

    revert decreasing candidate optimization

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/heuristicFinder/DecreasingHeuristicFinder.kt
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/heuristicFinder/DecreasingHeuristicFinder.kt
@@ -15,24 +15,17 @@ class DecreasingHeuristicFinder<S: State, A: Action, P: Prec>: HeuristicFinder<S
 		val argNode = astarNode.argNode
 
 		// tree parent still can be null if we get to an init node by a cover edge
-		val treeParentAstarNode = argNode.parent()?.let { astarArg[it] }
-
-		// This can probably be simplified by some proof but this is more robust
-		val candidates = mutableListOf<AstarNode<S, A>>()
-		candidates.addAll(argNode.coveredNodes().map{ astarArg[it] }.filter { it.heuristic.isKnown })
-		if (treeParentAstarNode != null && treeParentAstarNode.heuristic.isKnown) {
-			candidates += treeParentAstarNode
+		val treeParentAstarNode = argNode.parent()?.let { astarArg[it] }?.apply {
+			check(heuristic.isKnown)
 		}
 
-		// Take larger (= more precise) heuristic
-		val heuristicProvider = candidates.maxByOrNull { it.heuristic }
-
-		astarNode.heuristic = if (heuristicProvider == null) {
+		// TODO proof that we can't use covered nodes provider as they can be more specific not always providing a lowerbound
+		astarNode.heuristic = if (treeParentAstarNode == null) {
 			Distance.ZERO
 		} else {
 			Distance.finiteOf(
 				// 0 is always a known lowerbound, so it is more precise to use than negative numbers
-				max(heuristicProvider.heuristic.value - 1, 0)
+				max(treeParentAstarNode.heuristic.value - 1, 0)
 			)
 		}
 	}
