471d494955f563f857e13684d2d60cb2b528c9bd
asztrikx <asztrik35@gmail.com>
2023-10-08 21:01:54 +0200

    rename variable

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/Predicate.kt
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/Predicate.kt
@@ -8,15 +8,15 @@ infix fun <T> Predicate<T>.and(other: Predicate<T>): Predicate<T> = {
 
 /**
  * Creates a comparator which will order the elements so that
- * if an item matches a preceding predicate in the ordered predicate list [preds]
+ * if an item matches a preceding predicate in the ordered predicate list [predicates]
  * then it will be positioned earlier.
  */
-fun <T> predicateOrderedComparator(vararg preds: Predicate<T>) = Comparator<T> { t1, t2 ->
-	for (pred in preds) {
-		if (!pred(t1) && !pred(t2)) {
+fun <T> predicateOrderedComparator(vararg predicates: Predicate<T>) = Comparator<T> { t1, t2 ->
+	for (predicate in predicates) {
+		if (!predicate(t1) && !predicate(t2)) {
 			continue
 		}
-		return@Comparator compareByDescending<T> { pred(it) }.compare(t1, t2)
+		return@Comparator compareByDescending<T> { predicate(it) }.compare(t1, t2)
 	}
 	return@Comparator 0
 }
