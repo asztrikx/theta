ec5652263d3e294edc519de2064c844f4b16004a
asztrikx <asztrik35@gmail.com>
2023-10-23 11:34:35 +0200

    fix using newer commit

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/distanceSetter/NonFullDistanceSetter.kt
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/distanceSetter/NonFullDistanceSetter.kt
@@ -11,7 +11,7 @@ class NonFullDistanceSetter<S: State, A: Action, P: Prec>: DistanceSetter<S, A, 
 
 		// [reachedFinites] are expected to be ordered by distance
 		check(reachedFinites.map { distanceUntilTarget(it) }.zipWithNext { a, b -> a <= b }.all { it })
-		reachedFinites.filter { it.argNode.isTarget }.forEach { it.distance = Distance.F0 }
+		reachedFinites.filter { it.argNode.isTarget }.forEach { it.distance = Distance.ZERO }
 		reachedFinites.forEach { astarArg.propagateUpDistanceFromFiniteDistance(it, startNodes.toSet(), parents) }
 
 		if (startAstarNodes.none { it.distance.isFinite }) {
