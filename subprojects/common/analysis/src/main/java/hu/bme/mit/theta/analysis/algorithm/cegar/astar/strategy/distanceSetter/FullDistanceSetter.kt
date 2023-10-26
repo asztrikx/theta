daa3bb1f1c9b7b6f04d50898fd8ffbbc1b2a2767
asztrikx <asztrik35@gmail.com>
2023-10-26 15:15:29 +0200

    squash to [d050]

--- subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/distanceSetter/FullDistanceSetter.kt
+++ subprojects/common/analysis/src/main/java/hu/bme/mit/theta/analysis/algorithm/cegar/astar/strategy/distanceSetter/FullDistanceSetter.kt
@@ -9,7 +9,7 @@ import hu.bme.mit.theta.analysis.algorithm.cegar.astar.setDistanceFromAllTargets
 class FullDistanceSetter<S: State, A: Action, P: Prec>: DistanceSetter<S, A, P> {
 	override operator fun invoke(search: AstarSearch<S, A, P>) = search.run {
 		check(reachedFinites.all { it.argNode.isTarget })
-		astarArg.setDistanceFromAllTargets(reachedFinites.map { it.argNode })
+		astarArg.setDistanceFromAllTargets(reachedFinites)
 		check(startAstarNodes.any { it.distance.isFinite } || startAstarNodes.all { it.distance.isInfinite })
 	}
 }
